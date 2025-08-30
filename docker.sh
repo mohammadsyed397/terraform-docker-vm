0fbd84000fe98406fd8817a7d8ac1c7ccc30ba04
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-08-30 12:26:43 -0400

    latest

--- docker.sh
+++ docker.sh
@@ -13,3 +13,16 @@ lvextend -L +10G /dev/RootVG/varVol
 
 xfs_growfs /
 xfs_growfs /var
+
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
