53facad5f033048c3640eef530854596654358f9
mohammadsyed397 <mohammad.syed601@gmail.com>
2025-08-30 12:45:28 -0400

    latest

--- docker.sh
+++ docker.sh
@@ -14,15 +14,18 @@ lvextend -L +10G /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
 
-# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
-
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
-
-# (Optional) Verify checksum
-curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
-
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
-sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+
+eksctl version
+kubectl version
+
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
